0e1a7b08e0993b3664eac8fdabcd8440dc01fff6
Shiva Prasad <73731709+chennojushivaprasad@users.noreply.github.com>
2024-03-07 00:12:47 +0530

    Update uploadImageToCloudinary.js

--- MiddleWare/uploadImageToCloudinary.js
+++ MiddleWare/uploadImageToCloudinary.js
@@ -1,14 +1,24 @@
 const cloudinary = require("../config/CloudinaryConfig");
+const fs = require('fs');
 
-const uploadImageToCloudinary = async (imagePath,options={}) => {
+const uploadImageToCloudinary = async (imagePath, options = {}) => {
   try {
     if (imagePath) {
-      let result = await cloudinary.uploader.upload(imagePath,options);
-      return result
+      let result = await cloudinary.uploader.upload(imagePath, options);
+      
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          console.error(`Error deleting file: ${err}`);
+          return;
+        }
+        console.log('File deleted successfully');
+      });
+
+      return result;
     }
   } catch (error) {
     console.error(error);
-    throw new Error(error)
+    throw new Error(error);
   }
 };
 
