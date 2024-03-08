79d45a17077d3bee8a538aed10bf7b0607a3de20
Shiva Prasad <73731709+chennojushivaprasad@users.noreply.github.com>
2024-03-08 12:01:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,20 +11,11 @@ const connectDB = require('./config/mongoDBConfig');
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-const whitelist = ['http://localhost:3005',process.env.BASE_URL];
-
-const corsOptions = function (req, callback) {
-    let corsOptions;
-    if (whitelist.indexOf(req.headers.origin) !== -1) {
-        corsOptions = { origin: true };
-    } else {
-        corsOptions = { origin: false };
-    }
-
-    callback(null, corsOptions);
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+  })
+);
 
 
 const authRouter = require('./Routes/authRoutes');
